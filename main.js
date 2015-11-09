f8adc23dc26093fdd16461b7fff6b6adf2b60d14
ubershibs <luke..walker@gmail.com>
2015-11-09 00:10:39 -0500

    Finished project

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
-jQuery(document).ready(function(){
-	var row = '<div class="row"><ul><li></li><li></li><li></li><li></li></ul></div>';
-	var square = '<div class="square"></div>';
-	$('body').append('<div id="container"></div>');
-	$('#container').append(row);
-	$('#container').append(row);
-	$('#container').append(row);
-	$('#container').append(row);
-	$('li').append(square);
+var size = 4;
+
+function newSize() {
+	var size = prompt("How many squares per side?", "4");
+	if (size != null) {
+    	newGrid(size);	
+	}
+} 
+
+function newGrid(size) {
+	$('.square').remove();
+
+	for (i = 0; i < size; i++) {
+		var row = '<div>'
+
+		for (j = 0; j < size; j++) {
+			row +='<div class="square"></div>';
+		}
+
+		row += '</div>';
+
+		$('#container').append(row);
+	}
+	var calcSize = 960 / size + "px"
+	$('.square').css({"height": calcSize, "width": calcSize});
+}
+
+ $(document).ready(function(){
+	newGrid(size);
+	$('div').on('mouseenter', '.square', function(){
+		$(this).addClass('highlight');
+	});
+	$('button')
 });
